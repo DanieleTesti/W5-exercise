525d6a88a7d68b0af9be9c4f50332bf6a5ee6017
DanieleTesti <danieletesti14@gmail.com>
2023-01-22 23:10:33 +0100

    update

--- script.js
+++ script.js
@@ -1,4 +1,21 @@
-const contaScarpe = function () {
-  let nscarpe = scarpe.length;
-  alert("Al momento sul sito abbiamo " + nscarpe + " proposte disponibili");
-};
+function changeTheme() {
+  const el = document.documentElement;
+  el.getAttribute("data-theme") == "light"
+    ? el.setAttribute("data-theme", "dark")
+    : el.setAttribute("data-theme", "light");
+}
+
+let add = 0;
+function carrello() {
+  alert("Aggiunto al carrello correttamente");
+  add += 1;
+}
+
+function conta() {
+  alert("Nel carrello ci sono \n" + add + " \noggetti");
+}
+
+function svuota() {
+  add = 0;
+  alert("Ora il tuo carrello è vuoto");
+}
